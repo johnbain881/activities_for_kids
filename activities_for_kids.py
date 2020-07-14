f7cdc70db9e92d1b391e770da9332096c0deaefc
John Bain <johnknoxbain@gmail.com>
2020-07-14 14:18:01 -0500

    initial commit

---
+++ activities_for_kids.py
@@ -0,0 +1,24 @@
+def run(*names):
+  for name in names:
+    print(f"{name} ran like a fool!")
+
+
+def swing(*names):
+  for name in names:
+    print(f"{name} swung like a fool!")
+
+
+def slide(*names):
+  for name in names:
+    print(f"{name} slid like a fool!")
+
+
+def hide_and_seek(*names):
+  for name in names:
+    print(f"{name} hid like a fool!")
+
+
+run("Pam", "Sam", "Andrea", "Will")
+swing("Marybeth", "Ariel", "Kevin", "Courtney")
+slide("Mike", "Jack", "Jennifer", "Earl")
+hide_and_seek("Henry", "Heather", "Hayley", "Hugh")
